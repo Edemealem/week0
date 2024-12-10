14cfd2dbdfddd709cab2ed90d7c781053401464d
Edemealem <desuking1717@gmail.com>
2024-12-10 15:54:00 +0300

    .

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -1,47 +1,17 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-from google.colab import drive
-drive.mount('/content/drive')
 
 def load_data(file_path):
     return pd.read_csv(file_path)
 
-def missing_values(df, action='report', fill_value=None):
-    """
-    Check for missing values in the DataFrame.
-    
-    Parameters:
-    - df: pandas DataFrame
-    - action: 'report', 'drop', or 'fill' (default is 'report')
-    - fill_value: value to fill missing data if action is 'fill'
-    
-    Returns:
-    - DataFrame with missing values handled according to the specified action
-    """
-    if action == 'report':
-        return df.isnull().sum()
-    
-    elif action == 'drop':
-        return df.dropna()
-    
-    elif action == 'fill':
-        if fill_value is not None:
-            return df.fillna(fill_value)
-        else:
-            raise ValueError("fill_value must be provided when action is 'fill'")
-    
-    else:
-        raise ValueError("Invalid action. Choose 'report', 'drop', or 'fill'.")
+def missing_values(df):
+    return df.dropna()
 
 def clean_data(df):
-    """Clean the dataset by handling missing values and anomalies."""
-    
-    # Report missing values
-    missing_report = missing_values(df, action='drop')
+    missing_report = df.isnull().sum()
     print("Missing values report:\n", missing_report)
     
-
-    df = missing_values(df, action='fill', fill_value='No comments') 
+    df = missing_values(df)
+    
     for col in df.select_dtypes(include=['float64', 'int64']).columns:
         q1 = df[col].quantile(0.25)
         q3 = df[col].quantile(0.75)
@@ -51,15 +21,13 @@ def clean_data(df):
     return df
 
 def save_data(df, file_path):
-    """Save the cleaned dataset to a CSV file."""
     df.to_csv(file_path, index=False)
 
 def main():
-    # Paths to the datasets
     paths = {
-        'benin': '/content/drive/MyDrive/10Academy/benin-malanville.csv', 
-        'sierra_leone': '/content/drive/MyDrive/10Academy/sierraleone-bumbuna.csv',  
-        'togo': '/content/drive/MyDrive/10Academy/togo-dapaong_qc.csv'
+        'benin': "F:\\10 ACadamy data\\Dataset\\data\\benin-malanville.csv", 
+        'sierra_leone': "F:\\10 ACadamy data\\Dataset\\data\\sierraleone-bumbuna.csv",  
+        'togo': "F:\\10 ACadamy data\\Dataset\\data\\togo-dapaong_qc.csv"
     }
     
     for name, file_path in paths.items():
@@ -67,6 +35,7 @@ def main():
         df = load_data(file_path)
         cleaned_df = clean_data(df)
         save_data(cleaned_df, f'cleaned_{name}_data.csv')
+        print(f"Cleaned data saved as cleaned_{name}_data.csv")
 
 if __name__ == '__main__':
     main()
